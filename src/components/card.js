6c1cc35acef65c0e431aaf606c90c794f71769fb
theJOHNwatt <j.watt10@gmail.com>
2019-09-12 01:18:06 -0700

    imports and exports finished

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-export default Cards = props => {
+const Card = props => {
     return (
-        <></>
+        <div className='ApodCard'>
+            <h2>{props.title}</h2>
+            <h3>Taken By: {props.copyright}</h3>
+            <p>{props.date}</p>
+            <img src={props.url}/>
+            <h3>{props.explanation}</h3>
+            <a href={props.url}>Link To Image</a>
+        </div>
+        
     );
 };
+
+export default Card;
